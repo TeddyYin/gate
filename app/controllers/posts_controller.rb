b3b1917b8d4181510dcb03451cf2833f5b4a57b2
TeddyYin <ltboegod143@gmail.com>
2018-05-29 11:14:30 +0800

    implement update my post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create, :destroy]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -16,6 +16,22 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Update success"
+      # redirect_to account_posts_path, notice: "Update success"
+    else
+      flash[:alert] = "Update error"
+    end
+    redirect_to account_posts_path
   end
 
   def destroy
